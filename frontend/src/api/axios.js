642b9688c9486c0e7c37569a00edf7614a182f8c
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2026-05-07 23:14:12 +0530

    initial commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5000/api' || "https://crm-system-p769.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
